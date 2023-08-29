b34a0d46f53c944921d229b91b23364b5bf966ff
jakerios <jakerios01@gmail.com>
2023-08-29 17:56:48 -0400

    Created "My Picks" tab to view choices

--- website/views.py
+++ website/views.py
@@ -47,10 +47,7 @@ def home():
 
     testObject = sportsOdds()
     arr1, arr2, arr3, arr4 = testObject.getFootballOdds()
-    '''
-    for x in arr3:
-        if x <= 2:
-    '''
+
     return render_template("home.html", user=current_user, arr1=arr1, arr2=arr2, arr3=arr3, arr4=arr4)
 
 @views.route('/rules')
@@ -63,5 +60,45 @@ def odds():
     arr1, arr2, arr3, arr4 = oddsObject.getFootballOdds()
 
     return render_template("odds.html", user=current_user, arr1=arr1, arr2=arr2, arr3=arr3, arr4=arr4)
+
+@views.route('/myPicks')
+def myPicks():
+    if request.method == 'POST':
+        Note.query.delete()
+        db.session.commit()
+
+        '''Put the name of the select in as the string'''
+        game1Pick = request.form.get('game1Pick')
+        game2Pick = request.form.get('game2Pick')
+        game3Pick = request.form.get('game3Pick')
+        game4Pick = request.form.get('game4Pick')
+        game5Pick = request.form.get('game5Pick')
+        game6Pick = request.form.get('game6Pick')
+        game7Pick = request.form.get('game7Pick')
+        game8Pick = request.form.get('game8Pick')
+        game9Pick = request.form.get('game9Pick')
+        game10Pick = request.form.get('game10Pick')
+        game11Pick = request.form.get('game11Pick')
+        game12Pick = request.form.get('game12Pick')
+        game13Pick = request.form.get('game13Pick')
+        game14Pick = request.form.get('game14Pick')
+        game15Pick = request.form.get('game15Pick')
+        game16Pick = request.form.get('game16Pick')
+
+        new_picks =  Note(game1Pick = game1Pick, game2Pick = game2Pick, game3Pick = game3Pick, 
+                          game4Pick = game4Pick, game5Pick = game5Pick, game6Pick = game6Pick, 
+                          game7Pick = game7Pick, game8Pick = game8Pick, game9Pick = game9Pick, 
+                          game10Pick = game10Pick, game11Pick = game11Pick, game12Pick = game12Pick, 
+                          game13Pick = game13Pick, game14Pick = game14Pick, game15Pick = game15Pick, 
+                          game16Pick = game16Pick, user_id = current_user.id)
+        db.session.add(new_picks)
+        db.session.commit()
+
+        flash('Picks added', category='success')
+
+    testObject = sportsOdds()
+    arr1, arr2, arr3, arr4 = testObject.getFootballOdds()
+
+    return render_template("myPicks.html", user=current_user, arr1=arr1, arr2=arr2, arr3=arr3, arr4=arr4)
 
 
